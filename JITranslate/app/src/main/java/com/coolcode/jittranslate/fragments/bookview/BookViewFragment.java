81d89e5032b28dbea5f64921c37838f9ebc0abdb
saskamegaprogrammist <spiridonovaalex@gmail.com>
2020-04-01 16:50:20 +0300

    [INIT]

--- JITranslate/app/src/main/java/com/coolcode/jittranslate/fragments/bookview/BookViewFragment.java
+++ JITranslate/app/src/main/java/com/coolcode/jittranslate/fragments/bookview/BookViewFragment.java
@@ -1,4 +1,13 @@
 package com.coolcode.jittranslate.fragments.bookview;
 
-public class BookViewFragment {
+import androidx.fragment.app.Fragment;
+
+import com.coolcode.jittranslate.EventListenerActivity;
+
+public class BookViewFragment  extends Fragment {
+    private EventListenerActivity activity;
+
+    public void setEventListenerActivity(EventListenerActivity activity) {
+        this.activity = activity;
+    }
 }
